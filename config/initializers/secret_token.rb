d38b7a0a04eae527ca3ca4e007dafaa3bd033d42
Will Megson <wmegson@groupon.com>
2013-04-28 22:15:58 -0500

    first blogger commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '9d485a808071499de4c9718daafeefa6cf81937922de6138f48d920fff95ec67730f5efb17a30e130c4e56c0ec54e315d264aaf1bfffd489476850d607dcadec'
